cd3f7b0034063e7e9161c323ccb4d7d2ee1d4355
willcummings <will.cummings2@gmail.com>
2026-09-17 20:56:31 -0400

    Completed function 6 and passes all test cases

--- index.js
+++ index.js
@@ -125,7 +125,13 @@ export function shareLastDigit(num1, num2) {
  * isColdAndHot(10, 50); // false
  */
 export function isColdAndHot(temp1, temp2) {
-  /* TODO */
+  if (temp1 < 0 && temp2 > 100) {
+    return true;
+  } else if (temp2 < 0 && temp1 > 100) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /**
