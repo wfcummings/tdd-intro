99c0ac85815ebe79fa52572335d6077facc4a9b3
willcummings <will.cummings2@gmail.com>
2026-09-17 21:32:26 -0400

    Completed function 9 and passes all test cases

--- index.js
+++ index.js
@@ -194,7 +194,14 @@ export function makeSLS(str1, str2) {
  * canEnterClub(5, 5); // 1
  */
 export function canEnterClub(you, date) {
-  /* TODO */
+  const couple = you + date;
+  if (you <= 2 || date <= 2) {
+    return 0;
+  } else if (you >= 8 || date >= 8) {
+    return 2;
+  } else {
+    return 1;
+  }
 }
 
 /**
